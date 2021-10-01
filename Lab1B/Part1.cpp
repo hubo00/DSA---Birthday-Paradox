22965ac720ed63293bec7f2b9dba83c32da57f19
X00161897 Hubert Bukowski <x00161897@mytudublin.ie>
2021-10-01 18:00:41 +0100

    Initial Commit

---
+++ Lab1B/Part1.cpp
@@ -0,0 +1,49 @@
+/*
+Hubert Bukowski | x00161897 - CREATOR
+
+Worked with:
+Jibril Abdulazeez | x00164441
+Darius Crisan | x00165316
+Stephen Clarke | x00167225
+Daniel Cullen | x00164-
+
+*/
+#include <iostream>
+#include <random>
+#include <ctime>
+
+int main()
+{
+	int numIterations, numMatches;
+	int birthdays[50];
+	std::default_random_engine defEngine(time(0));
+	std::uniform_int_distribution<int> intDistro(1, 365);
+	std::cout << "Enter number of iterations: " << std::endl;
+	std::cin >> numIterations;
+
+	// Loop for 2-50 people, calculating the probability for each person each time
+	for (int peopleCount = 2; peopleCount <= 50; peopleCount++) {
+		numMatches = 0;
+		// Loop for the amount of specified iterations
+		for (int iteration = 0; iteration < numIterations; ++iteration) {
+			// Assign random birthdays to the amount of people currently in the loop
+			for (int person = 0; person < peopleCount; ++person) {
+				birthdays[person] = intDistro(defEngine);
+			}
+			// Loops for each person twice and compare birthdays to find matches
+			for (int person = 0; person < peopleCount; ++person) {
+				for (int person2 = 0; person2 < peopleCount; ++person) {
+					// Move into code block if person and person2 aren't the same
+					if (person != person2) {
+						if (birthdays[person] == birthdays[person2]) {
+							++numMatches;
+						}
+					}
+					break;
+				}
+			}
+		}
+		std::cout << "For " << peopleCount << " people, the probability of two birthdays is about " << float(numMatches) / float(numIterations) << std::endl;
+	}
+	return 0;
+}
